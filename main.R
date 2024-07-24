d98cc34fd3bb7cabc66462665797d92c6f73f2a8
dale-lonsdale <dlonsdale@outlook.com.au>
2024-07-24 15:14:16 +1000

    Update main.R

--- main.R
+++ main.R
@@ -7,6 +7,6 @@ source("SIP_library_definitions.R")
 #take arguments from command line
 args <- commandArgs(trailingOnly = TRUE)
 #user supplied variables
-user_supplied_data_name <- "example_user_data"
+user_supplied_data_name <- args[1]
 
 source("return_SIP_parameters.R")
