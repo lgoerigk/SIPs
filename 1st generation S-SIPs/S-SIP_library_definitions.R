059f479fa66efeefca4762b2605b1abde86dc6c6
dale-lonsdale <dlonsdale@outlook.com.au>
2024-07-25 12:25:42 +1000

    Add files via upload

---
+++ 1st generation S-SIPs/S-SIP_library_definitions.R
@@ -0,0 +1,79 @@
+#This file contains
+#a list of points (for S-SIPs only) that are not included due to convergence issues
+#names of the data files that contain the SIP library
+
+list_of_SIP_libraries <- c("S_SIP_0.001", "S_SIP_0.002", "S_SIP_0.005",
+                           "S_SIP_0.015", "S_SIP_0.02", "S_SIP_0.05", 
+                           "S_SIP_0.1", "S_SIP_0.5", "S_SIP_1", "S_SIP_2.5",
+                           "S_SIP_4", "S_SIP_5", "S_SIP_5", "S_SIP_10",
+                           "S_SIP_25", "S_SIP_40", "S_SIP_50", "S_SIP_100",
+                           "S_SIP_150", "S_SIP_200", "S_SIP_250", "S_SIP_300",
+                           "S_SIP_500")
+
+S_SIP_0.001_dataset_name <- "S_SIP_0.001.txt"
+S_SIP_0.001_degrees_of_freedom <- 1165
+
+S_SIP_0.002_dataset_name <- "S_SIP_0.002.txt"
+S_SIP_0.002_degrees_of_freedom <- 1165
+
+S_SIP_0.005_dataset_name <- "S_SIP_0.005.txt"
+S_SIP_0.005_degrees_of_freedom <- 1165
+
+S_SIP_0.015_dataset_name <- "S_SIP_0.015.txt"
+S_SIP_0.015_degrees_of_freedom <- 1165
+
+S_SIP_0.02_dataset_name <- "S_SIP_0.02.txt"
+S_SIP_0.02_degrees_of_freedom <- 1165
+
+S_SIP_150_dataset_name <- "S_SIP_150.txt"
+S_SIP_150_degrees_of_freedom <- 1165
+
+S_SIP_200_dataset_name <- "S_SIP_200.txt"
+S_SIP_200_degrees_of_freedom <- 1165
+
+S_SIP_300_dataset_name <- "S_SIP_300.txt"
+S_SIP_300_degrees_of_freedom <- 1165
+
+S_SIP_0.1_dataset_name <- "S_SIP_0.1.txt"
+S_SIP_0.1_degrees_of_freedom <- 1999
+
+S_SIP_0.05_dataset_name <- "S_SIP_0.05.txt"
+S_SIP_0.05_degrees_of_freedom <- 1999
+
+S_SIP_0.5_dataset_name <- "S_SIP_0.5.txt"
+S_SIP_0.5_degrees_of_freedom <- 1999
+
+S_SIP_1_dataset_name <- "S_SIP_1.txt"
+S_SIP_1_degrees_of_freedom <- 1999
+
+S_SIP_2.5_dataset_name <- "S_SIP_2.5.txt"
+S_SIP_2.5_degrees_of_freedom <- 1999
+
+S_SIP_4_dataset_name <- "S_SIP_4.txt"
+S_SIP_4_degrees_of_freedom <- 1999
+
+S_SIP_5_dataset_name <- "S_SIP_5.txt"
+S_SIP_5_degrees_of_freedom <- 1999
+
+S_SIP_10_dataset_name <- "S_SIP_10.txt"
+S_SIP_10_degrees_of_freedom <- 1999
+
+S_SIP_25_dataset_name <- "S_SIP_25.txt"
+S_SIP_25_degrees_of_freedom <- 1999
+
+S_SIP_40_dataset_name <- "S_SIP_40.txt"
+S_SIP_40_degrees_of_freedom <- 1999
+
+S_SIP_50_dataset_name <- "S_SIP_50.txt"
+S_SIP_50_degrees_of_freedom <- 1999
+
+S_SIP_100_dataset_name <- "S_SIP_100.txt"
+S_SIP_100_degrees_of_freedom <- 1999
+
+S_SIP_250_dataset_name <- "S_SIP_250.txt"
+S_SIP_250_degrees_of_freedom <- 1999
+
+S_SIP_500_dataset_name <- "S_SIP_500.txt"
+S_SIP_500_degrees_of_freedom <- 1999
+
+
